e245261a6e800716967e1f166a8148cb1ee6bf27
KARTHIK BK <158577841+Badamaranahalli123@users.noreply.github.com>
2026-02-10 15:15:00 +0530

    Refactor get_sheet function parameter name

--- sheets.py
+++ sheets.py
@@ -1,12 +1,11 @@
 import streamlit as st
-import pandas as pd
 import gspread
 from google.oauth2.service_account import Credentials
+import pandas as pd
 
-# Paste your actual Spreadsheet ID here
 SPREADSHEET_ID = "1QEfvt3IJr2g8GYq-n8YKtK6S69rnW-YC34r8Wb7YKsg"
 
-def get_sheet(sheet_name):
+def get_sheet(tab_name):
     scope = [
         "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive"
@@ -20,7 +19,7 @@ def get_sheet(sheet_name):
     client = gspread.authorize(creds)
 
     sheet = client.open_by_key(SPREADSHEET_ID)
-    worksheet = sheet.worksheet(sheet_name)
+    worksheet = sheet.worksheet(tab_name)
 
     data = worksheet.get_all_records()
     df = pd.DataFrame(data)
